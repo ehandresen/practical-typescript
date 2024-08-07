608caf80b95e715572661ce9e4f89427dbbf14db
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-08 00:06:10 +0200

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+const myString: string = 'Eirik';
+
+console.log(myString.toUpperCase());
+
+let num: number = 20;
+
+num = num * 2;
+
+console.log(num);
+
+const apple: boolean = true;
+
+if (apple) {
+  console.log('apple');
+}
+
+interface Person {
+  name: string;
+  skin: string;
+  male: boolean;
+}
+
+const user: Person = {
+  name: 'Eirik',
+  skin: 'caucation',
+  male: true,
+};
+
+console.log(user);
