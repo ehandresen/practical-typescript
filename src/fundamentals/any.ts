ec16b9a9797d3d61577661e1ee8926c5d120a96b
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-09 01:07:55 +0200

    type: any

---
+++ src/fundamentals/any.ts
@@ -0,0 +1,8 @@
+// this is like javascript works, be careful when to use any (it can spread like wildfire)
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false;
+
+console.log(typeof notSure); // boolean
+
+let random; // type is any ‹code›
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './style.css';
 import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.ts';
-import './union-type.ts';
+import './fundamentals/union-type.ts';
+import './fundamentals/any.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
