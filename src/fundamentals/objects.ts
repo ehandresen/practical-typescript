2bb5693555fba5d1dc38f9840608fce888cf7fb9
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-12 00:32:50 +0200

    optional property

--- src/fundamentals/objects.ts
+++ src/fundamentals/objects.ts
@@ -1,18 +1,16 @@
 // most likely would not set it up like this but its how it works under the hood
-let car1: car = {
+let car: { brand: string; year: number } = {
   brand: 'toyota',
   year: 2000,
-  model: 'one',
-  color: 'red',
-  fourWD: true,
 };
 
-interface car {
-  brand: string;
-  model: string;
-  year: number;
-  color: string;
-  fourWD: boolean;
-}
+console.log(car);
 
-console.log(car1);
+let book = { title: 'book', cost: 20 };
+let pen = { title: 'pen', cost: 10 };
+let notebook = { title: 'notebook' };
+
+// optional property (here 'cost')
+let items: { title: string; cost?: number }[] = [book, pen, notebook];
+
+console.log(items);
