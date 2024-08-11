46afb29f188241cf4a1a4defd5febc170a293839
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-12 00:46:58 +0200

    challenges objects

--- src/fundamentals/objects.ts
+++ src/fundamentals/objects.ts
@@ -13,3 +13,29 @@ let notebook = { title: 'notebook' };
 // optional property (here 'cost')
 let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
 items[0].title = 'new book'; // cannot change read-only property
+
+// Challenge
+
+// 1. Create an object bike of type { brand: string, year: number } and assign it some values. Then, try to assign a string to the year property.
+
+let bike: { brand: string; year: number } = {
+  brand: 'MX',
+  year: 2020,
+};
+
+bike.year = '2011';
+console.log(bike);
+
+// 2. Create an object laptop of type { brand: string, year: number } and try to assign an object with missing year property to it.
+
+let laptop: { brand: string; year: number } = {
+  brand: 'MacBook Air M1',
+};
+
+// 3. Create an array products of type { title: string, price?: number }[] and assign it some values. Then, try to add an object with a price property of type string to it.
+
+const bed = { title: 'bed', price: 100 };
+const tv = { title: 'bed', price: 100 };
+const lamp = { title: 'bed', price: '20' };
+
+let products: { title: string; price: number }[] = [bed, tv, lamp];
