f47c718dd074d5f8fd36734fbdf27a76e03ed995
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-13 15:12:14 +0200

    challenges functions

--- src/fundamentals/function.ts
+++ src/fundamentals/function.ts
@@ -1,5 +1,17 @@
-function sayHi(name: string) {
-  console.log(`Hello ${name.toUpperCase()}`);
+function calculateDiscount(price: number): number {
+  return price * 0.9;
 }
 
-sayHi('Tim');
+const finalPrice = calculateDiscount(48); // finalPrice is now inferred as a number
+
+console.log(finalPrice);
+
+// Challenge
+
+let names = ['John', 'Tim', 'Veronica', 'Jessica'];
+
+function isNameInList(name: string): boolean {
+  return names.includes(name);
+}
+
+console.log(isNameInList('Tim')); // true
