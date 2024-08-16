8d6f041537d36f163388bbe6cd8f0501024680be
ehandresen <eirik.horgen.andresen@gmail.com>
2024-08-16 13:43:53 +0200

    type unknown

---
+++ src/fundamentals/type/type-unknown.ts
@@ -0,0 +1,34 @@
+let unknownValue: unknown;
+
+// Assign different types of values to unknownValue
+unknownValue = 'Hello World'; // OK
+unknownValue = [1, 2, 3]; // OK
+unknownValue = 42.3344556; // OK
+
+// unknownValue.toFixed( ); // Error: Object is of type 'unknown'
+
+// Now, let's try to use unknownValue
+if (typeof unknownValue === 'number') {
+  // TypeScript knows that unknownValue is a string in this block
+  console.log(unknownValue.toFixed(2)); // OK
+}
+
+function runSomeCode() {
+  const random = Math.random();
+  if (random < 0.5) {
+    throw new Error('Something went wrong');
+  } else {
+    throw 'some error';
+  }
+}
+
+try {
+  runSomeCode();
+} catch (error) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  } else {
+    console.log(error);
+    console.log('there was an error....');
+  }
+}
